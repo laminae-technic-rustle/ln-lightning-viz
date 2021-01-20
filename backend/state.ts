77f37d2898ebebbf326802ae1dc3996edb3ff4b9
laminae-technic-rustle <oizys-curarum@protonmail.com>
2021-01-20 23:22:17 +0100

    :construction: - V2.
    - Data is now de-duplicated, 7 fold less edges, much better rendering
    - Using for-loops now instead of functional stuff in the BE, is a lot
    faster.
    - Added metadata and endpoints to get nodes / edges / edges-by-node
      - Metadata contains average, median, min, max connections per node

--- backend/state.ts
+++ backend/state.ts
@@ -1,48 +1,87 @@
 import cliProgress from "cli-progress";
 import worker from "worker_threads";
 
-import type { jsonGraph, jsonNode, jsonEdge, graph, node, edge } from "shared";
+import type { metadata, jsonGraph, jsonNode, jsonEdge, graph, node, edge } from "shared";
 
 const jGraph: jsonGraph = require("../lightning_graph.json");
 
 type state = {
   graph: graph,
+  metadata: metadata,
   nodes: { [key: string]: jsonNode },
   edges: { [key: string]: jsonEdge }
 };
 
 const progress = {
   node: new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic),
-  edge: new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
+  edge: new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic),
+  meta: new cliProgress.SingleBar({}, cliProgress.Presets.shades_classic)
 };
 
 const initState = (): state => {
-  console.log("\n **** Started - Parsing Nodes **** \n");
+  console.log("\n **** Parsing Nodes ****");
   progress.node.start(jGraph.nodes.length - 1, 0);
 
-  const nodeArray = jGraph.nodes.reduce((acc, jsonN: jsonNode, i) => {
+  const nodes: { [key: string]: jsonNode } = {};
+  const nodeArray: node[] = [];
+  for (let i = 0; i < jGraph.nodes.length; i++) {
     progress.node.update(i);
-    return [...acc, [jsonN.pub_key, jsonN.alias]];
-  }, []);
-
+    const node = jGraph.nodes[i];
+    const { pub_key, alias } = node;
+    nodeArray.push([pub_key, alias])
+    nodes[pub_key] = node;
+  };
   progress.node.stop();
-  console.log("\n **** Finished - Parsing Nodes **** \n");
 
-  console.log("\n **** Started - Parsing Edges **** \n");
+  console.log("\n **** Started - Parsing Edges ****");
   progress.edge.start(jGraph.edges.length - 1, 0);
 
-  const edgeArray = jGraph.edges.reduce((acc, jsonE: jsonEdge, i) => {
+  const edges: { [key: string]: jsonEdge } = {}
+  const edgeArray: edge[] = [];
+  for (let i = 0; i < jGraph.edges.length; i++) {
     progress.edge.update(i);
-    return [...acc, [jsonE.channel_id, jsonE.node1_pub, jsonE.node2_pub]];
-  }, []);
-
+    const edge = jGraph.edges[i];
+    const { channel_id, node1_pub, node2_pub } = edge;
+    edgeArray.push([channel_id, node1_pub, node2_pub])
+    edges[channel_id] = edge;
+  };
   progress.edge.stop();
-  console.log("\n **** Finished - Parsing Edges **** \n");
+
+  console.log("\n **** Started - Parsing Metadata ****");
+  progress.meta.start(edgeArray.length - 1, 0);
+
+  const nodeConnections: { [key: string]: number } = {};
+  for (let i = 0; i < edgeArray.length; i++) {
+    progress.meta.update(i);
+
+    const from = edgeArray[i][1];
+    const oldFrom = nodeConnections[from];
+    nodeConnections[from] = typeof oldFrom === "undefined" ? 0 : oldFrom + 1;
+
+    const to = edgeArray[i][2];
+    const oldTo = nodeConnections[to];
+    nodeConnections[to] = typeof oldTo === "undefined" ? 0 : oldTo + 1;
+  };
+
+  const numbersOnly = Object.values(nodeConnections).sort();
+  const average = numbersOnly.reduce((acc, x) => x + acc, 0) / numbersOnly.length;
+  const median = numbersOnly[Math.round(numbersOnly.length / 2)];
+  const min = numbersOnly[0]
+  const max = numbersOnly[numbersOnly.length - 1];
+
+  progress.meta.stop();
 
   return {
     graph: { nodes: nodeArray, edges: edgeArray },
-    nodes: {}, // TODO - Add this
-    edges: {} // TODO - Add this
+    metadata: {
+      nodeConnections,
+      min,
+      max,
+      average,
+      median,
+    },
+    nodes,
+    edges
   };
 };
 
